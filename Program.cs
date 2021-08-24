a93860d4e027c67934492b70b8122c2ab92d5b6b
Phil Busch <phil.busch@gmail.com>
2021-08-23 20:58:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.WriteLine("Press a key");
+    var key = Console.ReadLine();
+    if (key == null) continue;
+    if (key.Equals("Y"))
+    {
+        Console.WriteLine("You pressed Y");
+    }
+    else
+    {
+        Console.WriteLine("You pressed another key");
+    }
+}
